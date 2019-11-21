4c94b65b7f0a484d3569704dd1a500e20f688b62
Chama SALMI <salmichama@gmail.com>
2019-11-21 11:22:47 +0100

    update RestController

--- src/main/java/com/adaming/animals/rest/RestController.java
+++ src/main/java/com/adaming/animals/rest/RestController.java
@@ -1,12 +1,16 @@
 package com.adaming.animals.rest;
 
 import com.adaming.animals.entity.Animals;
+import com.adaming.animals.entity.Organs;
 import com.adaming.animals.service.AnimalsServiceImpl;
 import com.adaming.animals.service.OrgansServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.web.bind.annotation.RestController
 
@@ -23,5 +27,19 @@ public class RestController {
         Animals animals = animalsService.findById(idToShow);
         return animals;
     }
+
+    @GetMapping("/organ")
+    public Organs nameOrgan(@RequestParam(name = "name", required = false)  String name, Model model) {
+        Organs organ = organsService.showSpecificOrgan(name);
+
+        if (organ == null) {
+            model.addAttribute("organNotFound", true);
+        } else {
+            model.addAttribute("organ", organ);
+        }
+        return organ;
+    }
+
+
 
 }
